ad7ff465e2bcfe7bb7cb2cad8c1662e2fd215366
Pitayafruit <1037153356@qq.com>
2023-04-19 23:57:14 +0800

    feat:add content api swagger

--- xuecheng-plus-base/src/main/java/com/xuecheng/base/model/PageParms.java
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/model/PageParms.java
@@ -1,6 +1,7 @@
 package com.xuecheng.base.model;
 
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.ToString;
 
@@ -10,10 +11,13 @@ import lombok.ToString;
 @Data
 @ToString
 public class PageParms {
+
     //当前页码
+    @ApiModelProperty("当前页码")
     private Long pageNo = 1L;
 
     //每页记录数默认值
+    @ApiModelProperty("每页记录数默认值")
     private Long pageSize =10L;
 
     public PageParms() {
--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -4,18 +4,32 @@ import com.xuecheng.base.model.PageParms;
 import com.xuecheng.base.model.PageResult;
 import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 课程信息编辑接口
  */
+@Api(value = "课程信息编辑接口",tags = "课程信息编辑接口")
 @RestController
 public class CourseBaseInfoController {
 
+    @ApiOperation("课程查询接口")
     @RequestMapping("/course/list")
     public PageResult<CourseBase> list(PageParms pageParms, @RequestBody(required = false) QueryCourseParamsDto queryCourseParamsDto){
-        return null;
+        CourseBase courseBase = new CourseBase();
+        courseBase.setName("测试名称");
+        courseBase.setCreateDate(LocalDateTime.now());
+        List<CourseBase> courseBases = new ArrayList<>();
+        courseBases.add(courseBase);
+        PageResult<CourseBase> pageResult = new PageResult<>(courseBases,10,1,10);
+        return pageResult;
     }
 }
