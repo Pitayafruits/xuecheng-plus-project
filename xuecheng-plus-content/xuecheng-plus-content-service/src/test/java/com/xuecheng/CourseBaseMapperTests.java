f8931456ba859c55b084322a50685ee2a7f06a36
Pitayafruit <1037153356@qq.com>
2023-04-20 11:33:50 +0800

    feat:add content service mapper test

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/CourseBaseMapperTests.java
@@ -0,0 +1,58 @@
+package com.xuecheng;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+
+import com.xuecheng.base.model.PageParms;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.mapper.CourseBaseMapper;
+import com.xuecheng.content.model.dto.QueryCourseParamsDto;
+import com.xuecheng.content.model.po.CourseBase;
+import io.swagger.annotations.Api;
+import org.apache.commons.lang.StringUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+
+public class CourseBaseMapperTests {
+
+    @Autowired
+    CourseBaseMapper courseBaseMapper;
+
+    @Test
+    void testCouseBaseMapper(){
+        CourseBase courseBase = courseBaseMapper.selectById(74L);
+        Assertions.assertNotNull(courseBase);
+
+        //测试查询接口
+        LambdaQueryWrapper<CourseBase> queryWrapper = new LambdaQueryWrapper<>();
+        //查询条件
+        QueryCourseParamsDto queryCourseParamsDto = new QueryCourseParamsDto();
+        queryCourseParamsDto.setCourseName("java");
+        queryCourseParamsDto.setAuditStatus("202004");
+        queryCourseParamsDto.setPublishStatus("203001");
+        //拼接查询条件
+        queryWrapper.like(StringUtils.isNotEmpty(queryCourseParamsDto.getCourseName()),CourseBase::getName,queryCourseParamsDto.getCourseName());
+        //根据课程审核状态
+        queryWrapper.eq(StringUtils.isNotEmpty(queryCourseParamsDto.getAuditStatus()),CourseBase::getAuditStatus,queryCourseParamsDto.getAuditStatus());
+        //分页参数
+        PageParms pageParms = new PageParms();
+        pageParms.setPageNo(1L);
+        pageParms.setPageSize(3L);
+        Page<CourseBase> page = new Page<>(pageParms.getPageNo(),pageParms.getPageSize());
+        //查询
+        Page<CourseBase> pageResult = courseBaseMapper.selectPage(page, queryWrapper);
+        //数据
+        List<CourseBase> records = pageResult.getRecords();
+        //总记录数
+        long total = pageResult.getTotal();
+        //准备返回数据
+        PageResult<CourseBase> courseBasePageResult = new PageResult<>(records,total,pageParms.getPageNo(),pageParms.getPageSize());
+        System.out.println(courseBasePageResult);
+    }
+}
