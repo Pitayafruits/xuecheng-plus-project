67eb12cd9092fdb8f2c718ff4f8ba3a3fa67258a
Pitayafruit <1037153356@qq.com>
2023-04-27 14:31:21 +0800

    feat:add JSR303 validation

--- xuecheng-plus-base/src/main/java/com/xuecheng/base/exception/GlobalExceptionHandler.java
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/exception/GlobalExceptionHandler.java
@@ -1,12 +1,18 @@
 package com.xuecheng.base.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 全局异常处理
@@ -24,7 +30,6 @@ public class GlobalExceptionHandler {
     public RestErrorResponse customException(XueChengPlusException e) {
         log.error("【系统异常】{}", e.getErrMessage(), e);
         return new RestErrorResponse(e.getErrMessage());
-
     }
 
     @ResponseBody
@@ -35,6 +40,20 @@ public class GlobalExceptionHandler {
         log.error("【系统异常】{}", e.getMessage(), e);
 
         return new RestErrorResponse(CommonError.UNKOWN_ERROR.getErrMessage());
-
     }
+
+    @ResponseBody
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public RestErrorResponse methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        List<String> msgList = new ArrayList<>();
+        //将错误信息放在msgList
+        bindingResult.getFieldErrors().stream().forEach(item->msgList.add(item.getDefaultMessage()));
+        //拼接错误信息
+        String msg = StringUtils.join(msgList, ",");
+        log.error("【系统异常】{}",msg);
+        return new RestErrorResponse(msg);
+    }
+
 }
